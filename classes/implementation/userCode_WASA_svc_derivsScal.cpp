8814336943f43455622a35bb57efc0d5d7923e27
Tobias Pilz <tpilz@uni-potsdam.de>
2017-12-04 13:49:18 +0100

    ENH: Changed WASA_svc parameter 'wc_etmax' to 'f_etmax' including description. However, internally the parameter already had been used as it is described now.

--- classes/implementation/userCode_WASA_svc_derivsScal.cpp
+++ classes/implementation/userCode_WASA_svc_derivsScal.cpp
@@ -217,7 +217,7 @@ et_act (
 	wcs_root,									// Volumetric water content at saturation of the root zone (m3/m3)
 	wcp_root,									// Volumetric water content of the root zone at permanent wilting point (m3/m3)
 	wcr_root,									// Residual volumetric water content of the root zone (m3/m3)
-	paramNum(wc_etmax)*wcf_root,								// Parameter giving the volumetric water content where et_act equals et_pot, typically wc_etmax / wc_fk = [0.5..0.8] (m3/m3)
+	paramNum(f_etmax)*wcf_root,								// Parameter giving the volumetric water content where et_act equals et_pot, typically wc_etmax / wc_fk = [0.5..0.8] (m3/m3)
 	bubble_root*100.,									// Bubbling pressure of the root zone (cm) or (hPa)
 	porei_root,								// Pore-size-index of the root zone (-)
 	paramNum(wstressmin)*100.,							// Threshold for water stress effect on resistance (begin of stomata closure) (m) OR (100 hPa)
